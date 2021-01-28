b665d4df79fcbebb7199f071d4af8c5b5fe86c6c
AlexanderSh <38387356+ashekshuev@users.noreply.github.com>
2021-01-29 01:16:07 +0300

    Добавление контроллера.

---
+++ CodeBlogFitness.BL/Controller/UserController.cs
@@ -0,0 +1,17 @@
+﻿using CodeBlogFitness.BL.Model;
+using System;
+
+namespace CodeBlogFitness.BL.Controller
+{
+	public class UserController
+	{
+		public User User { get; }
+
+		public UserController(User user)
+		{
+			User = user ?? throw new ArgumentNullException("Пользователь не может быть равен null",nameof(user));
+		}
+
+		public bool Save();
+	}
+}
